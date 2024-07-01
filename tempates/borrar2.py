1f54368a28ea749e8307776196c47cc0d3ddc3ca
jhonnferrerosa <jhonn.trucke@gmail.com>
2024-07-01 13:06:14 +0200

    first commit

---
+++ tempates/borrar2.py
@@ -0,0 +1,22 @@
+
+from flask import Flask
+
+app = Flask(__name__);  
+
+
+
+@app.route('/')   
+def index():
+	return ("holaMundo desde flasksssasdfasdfasdfs.");
+
+
+
+
+
+if __name__ == '__main__':
+
+
+
+
+	app.run(debug=True, port=8888); 
+
