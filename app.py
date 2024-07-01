14efcb7289d0179aa61d85eb783fd4f0bbeab1cb
jhonnferrerosa <jhonn.trucke@gmail.com>
2024-07-01 12:48:54 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+
+
+
+from flask import Flask
+
+app = Flask(__name__);  
+
+
+
+@app.route('/')   
+def index():
+	return ("holaMundo desde flasks.");
+
+
+
+
+
+if __name__ == '__main__':
+
+
+
+
+	app.run(debug=True, port=8888); 
+
+
+
