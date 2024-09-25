777b4b7b909c889ad1f64036bed21480409d1fc9
jhonnferrerosa <jhonn.trucke@gmail.com>
2024-09-25 18:42:34 +0200

    la contraseña tiene que acabar en exclamacion, no me vale el @ por el postgres

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class DevelopmentConfig ():
     DEBUG = True;
     PORT=5000;
     SECRET_KEY = 'miPalabraSecreta';  # esto hasta que no use un formulario con csrf token, no lo puedo usar.  Mirar si esto se puede sacar del sistema operativo ya que es una clave que tiene que estar segura.
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Grandesvirus2@!!@localhost/z';
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Grandesvirus2!@localhost/z';
     
 
 
