d14d0cffdc0e9d794b82936db3153dfdc78c2cf7
jhonnferrerosa <jhonn.trucke@gmail.com>
2024-09-26 10:20:06 +0200

    en este commit he quitado el CSRF token para que pueda depurar el codigo desde VSC

--- config.py
+++ config.py
@@ -13,6 +13,7 @@ class DevelopmentConfig ():
     PORT=5000;
     SECRET_KEY = 'miPalabraSecreta';  # esto hasta que no use un formulario con csrf token, no lo puedo usar.  Mirar si esto se puede sacar del sistema operativo ya que es una clave que tiene que estar segura.
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Grandesvirus2!@localhost/z';
+
     
 
 
